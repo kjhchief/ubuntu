67628128d8f7cae550f05b42d9a104b72d8b5606
kjhchief <kjhchief@naver.com>
2023-08-23 10:20:57 +0000

    230823_3

---
+++ hongongC/18-9.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	FILE *ifp, *ofp;
+	char name[20];
+	int kor, eng, math;
+	int total;
+	double avg;
+	int res;
+
+	ifp = fopen("a.txt", "r");
+	if (ifp == NULL)
+	{
+	}
+}
