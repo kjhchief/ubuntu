9ef32a69d2543df60e1a8ff61ebaf9d84770ec13
kjhchief <kjhchief@naver.com>
2023-09-22 06:17:20 +0000

    230920

---
+++ hongongC/18-2check2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char str[5];
+	char ch;
+	int i = 0;
+	ch = fgetc(stdin);
+	while ((i < (sizeof(str) - 1)) && (ch != '\n'))
+	{
+		str[i++] = ch;
+		ch = fgetc(stdin);
+	}
+	if (i < (sizeof(str) - 1)) str[i++] = ch;
+	str[i] = '\0';
+
+	return 0;
+}
